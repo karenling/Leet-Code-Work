532870f681615949c4a41e0fb5dbba657f54a02b
Karen Ling <karensling@gmail.com>
2015-07-20 19:34:07 -0700

    Additional solution for majority element.
    Slower because it requires a check at every iteration.

--- majority_element.rb
+++ majority_element.rb
@@ -1,7 +1,12 @@
+# runtime 108ms
 def majority_element(nums)
   counter = Hash.new(0)
+  majority_num = nums.length / 2
   nums.each do |el|
     counter[el] += 1
+    if counter[el] > majority_num
+      return el
+    end
   end
   majority_num = nums.length / 2
   majority = counter.select do |k, v|
@@ -9,6 +14,19 @@ def majority_element(nums)
   end
 
   majority.first.first
+end
+
+
+# runtime 144ms
+def majority_element(nums)
+  counter = Hash.new(0)
+  majority_num = nums.length / 2
+  nums.each do |el|
+    counter[el] += 1
+    if counter[el] > majority_num
+      return el
+    end
+  end
 end
 
 p majority_element([1, 2, 3, 2, 2]) # 2
