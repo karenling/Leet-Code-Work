4966c1d2403724445e6540b3d0f57696b58c7bfc
Karen Ling <karensling@gmail.com>
2015-07-23 01:36:52 -0700

    Complete reverse integer.

--- reverse_integer.rb
+++ reverse_integer.rb
@@ -1,20 +1,27 @@
 # @param {Integer} x
 # @return {Integer}
+# 92ms runtime
+def rev(n)
+  reversed = []
+  until n == 0
+    reversed << n % 10
+    n = n / 10
+  end
+  reversed.join("").to_i
+end
+
+p rev(234)
+
 def reverse(x)
   val = nil
   if x < 0
     x *= -1
-    val = x.to_s.split("").reverse.join("").to_i * -1
+    val = rev(x) * -1
+  else
+    val = rev(x)
+  end
 
-  else
-    val = x.to_s.split("").reverse.join("").to_i
-  end
-  
-  if val > 2147483648 || val < -2147483648
-    return 0
-  else
-    val
-  end
+  (val > 2147483648 || val < -2147483648) ? 0 : val
 end
 
 p reverse(123) # 123
